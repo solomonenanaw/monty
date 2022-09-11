2606c77a5bd3928d2f16fc2943644bc8115b2462
solomonenanaw <sekewbeg@gmail.com>
2022-09-11 06:46:13 +0000

    header

--- main.c
+++ main.c
@@ -1,47 +1,53 @@
 #include "monty.h"
 
-vars var;
-
 /**
- * main - Start LIFO, FILO program
- * @ac: Number of arguments
- * @av: Pointer containing arguments
- * Return: 0 Success, 1 Failed
+ *  main - Main
+ *
+ *  @argc: Number of args
+ *
+ *  @argv: Command line args
+ *
+ *  Return: Void
  */
-int main(int ac, char **av)
+
+int main(int argc, char *argv[])
 {
-	char *opcode;
+	stack_t *head = NULL;
+	char  *str = NULL, *operator_array[2], *temp;
+	size_t bufsize = 1024, line_count = 0;
+	ssize_t get_line;
+	void (*operator_function)(stack_t **stack, unsigned int line_number);
 
-	if (ac != 2)
+if (argc != 2)
+	fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
+file = fopen(argv[1], "r");
+if (file == NULL)
+	fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
+while (1)
+{
+	get_line = getline(&str, &bufsize, file);
+	if (get_line == -1)
+		break;
+	line_count++;
+	operator_array[0] = strtok(str, "\n ");
+	if (operator_array[0] == NULL)
+		get_nop(&head, line_count);
+	else if (strcmp("push", operator_array[0]) == 0)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
-	}
-
-	if (start_vars(&var) != 0)
-		return (EXIT_FAILURE);
-
-	var.file = fopen(av[1], "r");
-	if (!var.file)
+																	temp = strtok(NULL, "\n ");
+																																get_push(&head, line_count, temp);
+																															}
+	else if (operator_array[0] != NULL && operator_array[0][0] != '#')
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-		free_all();
-		return (EXIT_FAILURE);
+																																operator_function = go(operator_array[0], line_count, &head);
+																																if (operator_function == NULL && line_count == 0)
+																	{
+																		fprintf(stderr, "L%ld: unknown instruction %s\n",
+																			line_count, operator_array[0]), exit(EXIT_FAILURE);
+																	}
+		operator_function(&head, line_count);
 	}
-
-	while (getline(&var.buff, &var.tmp, var.file) != EOF)
-	{
-		opcode = strtok(var.buff, " \r\t\n");
-		if (opcode != NULL)
-			if (call_funct(&var, opcode) == EXIT_FAILURE)
-			{
-				free_all();
-				return (EXIT_FAILURE);
-			}
-		var.line_number++;
-	}
-
-	free_all();
-
-	return (EXIT_SUCCESS);
+}
+fclose(file), free(str), get_free(head);
+return (0);
 }
